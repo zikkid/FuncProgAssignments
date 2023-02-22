dfb818a9bf0098674c042edc49e3dc47d09f95bf
Viggo Starcke <vist@itu.dk>
2023-02-22 11:30:27 +0100

    4.1 G

---
+++ Assignment4/Assignment4/MultiSet.fs
@@ -0,0 +1,20 @@
+﻿module MultiSet
+     type MultiSet<'a> when 'a : comparison = M of Map<'a, uint32>
+     
+     let empty = Map.empty<'a, uint32> //Map.<'a, uint32> []
+     let isEmpty (M s) = Map.isEmpty s
+     let size (M s) = Map.count s
+     let contains a (M s) = Map.containsKey a s
+     let numItems a (M s) =
+        match Map.tryFind a s with
+        | _ -> true
+     let add a n (M s) =
+         let an = Map.find a s
+         Map.add a (an + n) s
+     let addSingle a (M s) = Map.add a s
+     let remove a n (M s) =
+        let an = Map.find a s
+        Map.add a (-(an + n)) s
+     let removeSingle a (M s) = Map.remove a s
+     let fold f acc (M s) = Map.fold f acc s
+     let foldBack f acc (M s) = Map.foldBack f s acc
--- Assignment4/Assignment4/MultiSet.fsi
+++ Assignment4/Assignment4/MultiSet.fsi
@@ -1,2 +1,16 @@
-﻿module Assignment4.MultiSet
-
+﻿module MultiSet
+    type MultiSet<'a> when 'a : comparison
+    
+    val empty : MultiSet<'a>
+    val isEmpty : MultiSet<'a> -> bool
+    val size : MultiSet<'a> -> uint32
+    val contains : 'a -> MultiSet<'a> -> bool
+    val numItems : 'a -> MultiSet<'a> -> uint32
+    val add : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> 
+    val addSingle : 'a -> MultiSet<'a> -> MultiSet<'a>
+    val remove : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a>
+    val removeSingle : 'a -> MultiSet<'a> -> MultiSet<'a>
+    val fold : ('a -> 'b -> uint32 -> 'a) -> 'a -> MultiSet<'b> -> 'a
+    val foldBack : ('a -> uint32 -> 'b -> 'b) -> MultiSet<'a> -> 'b -> 'b
+    
+    
