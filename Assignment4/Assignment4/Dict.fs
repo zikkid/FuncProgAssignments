87a6ae4802fb85c0828384896ff6540167a14721
zikkid <viggost4rcke@gmail.com>
2023-02-23 21:39:15 +0100

    4.2 started

---
+++ Assignment4/Assignment4/Dict.fs
@@ -0,0 +1,16 @@
+﻿module Dict
+    type Dict =
+        | Leaf of bool
+        | Node of bool * Map<char, Dict>
+        
+    let empty = Leaf false
+    
+    let insert x =
+        | ??
+        
+    // let lookup x =
+    //     | ??
+    //     
+    // let step x =
+    //     | ??
+
--- Assignment4/Assignment4/MultiSet.fs
+++ Assignment4/Assignment4/MultiSet.fs
@@ -1,6 +1,7 @@
 ﻿module MultiSet
      type MultiSet<'a when 'a : comparison> = M of Map<'a, uint32>
      
+     //Green
      let empty = M Map.empty
      let isEmpty (M s) = Map.isEmpty s
      let size (M s) = uint32 (Map.count s) 
@@ -21,5 +22,7 @@
      let removeSingle a (M s) = Map.remove a s |> M
      let fold f acc (M s) = Map.fold f acc s
      let foldBack f (M s) acc = Map.foldBack f s acc
-     let ofList lst = List.map lst
-     let toList (M s) = Map.toList s
+     
+     //Yellow
+     // let ofList lst = List.map lst
+     // let toList (M s) = Map.toList s
