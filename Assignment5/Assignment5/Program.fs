fdceec915151599ae6ecd03bbeac3e31f70f466f
zikkid <viggost4rcke@gmail.com>
2023-03-04 23:39:04 +0100

    5.2

---
+++ Assignment5/Assignment5/Program.fs
@@ -0,0 +1,18 @@
+﻿//Exercise 5.1
+let rec sum acc n =
+    match n with
+    | 0 -> acc
+    | _ -> sum (acc + n) (n - 1)
+
+
+//Exercise 5.2
+let rec length acc lst =
+    match lst with
+    | [] -> acc
+    | x::xs -> length (acc + 1) xs
+    
+//Exercise 5.3
+let rec foldBack f acc lst =
+    match lst with
+    | [] -> f acc
+    | x::xs -> foldBack f () xs
