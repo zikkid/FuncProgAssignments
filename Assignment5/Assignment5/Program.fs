bcc2f529d0c6a23c896587167da7894f4aac647b
zikkid <viggost4rcke@gmail.com>
2023-03-05 00:05:38 +0100

    5.2

--- Assignment5/Assignment5/Program.fs
+++ Assignment5/Assignment5/Program.fs
@@ -6,13 +6,27 @@ let rec sum acc n =
 
 
 //Exercise 5.2
-let rec length acc lst =
-    match lst with
-    | [] -> acc
-    | x::xs -> length (acc + 1) xs
+let length lst =
+    let rec lengthA acc lst =
+        match lst with
+        | [] -> acc
+        | x::xs -> lengthA (acc + 1) xs
+    lengthA 0 lst
     
 //Exercise 5.3
 let rec foldBack f acc lst =
     match lst with
     | [] -> f acc
-    | x::xs -> foldBack f () xs
+    | x::xs -> foldBack f (foldBack f xs acc)
+//Need help here
+
+//Exercise 5.4
+let factA x =
+    let rec aux acc =
+        function
+        | 0 -> acc
+        | x -> aux (x * acc) (x - 1)
+    aux 1 x
+    
+let factC x =
+    let rec aux 
