a82c110960609d5d28310c5bf56b957167cb865c
Viggo Starcke <vist@itu.dk>
2023-03-14 20:20:49 +0100

    5 yellow

--- Assignment4/Assignment4/MultiSet.fs
+++ Assignment4/Assignment4/MultiSet.fs
@@ -1,32 +1,41 @@
 ﻿module MultiSet
-     type MultiSet<'a when 'a : comparison> = M of Map<'a, uint32>
+    type MultiSet<'a when 'a : comparison> = M of Map<'a, uint32>
      
-     //Green
-     let empty = M Map.empty
-     let isEmpty (M s) = Map.isEmpty s
-     let size (M s) = uint32 (Map.count s) 
-     let contains a (M s) = Map.containsKey a s
-     let numItems a (M s) =
+    //Green
+    let empty = M Map.empty
+    
+    let size (M s) = Map.fold (fun x k v -> x + v) 0u s
+    let isEmpty m =
+        match size m with
+        | 0u -> true
+        | _ -> false
+    let contains a (M s) = Map.containsKey a s
+    let numItems a (M s) =
         match Map.tryFind a s with
         | None -> 0u
         | Some x -> x
-     let add a n (M s) =
-         match Map.tryFind a s with
-         | Some an -> Map.add a (an + n) s |> M
-         | None -> Map.add a n s |> M
-     
-         
-     
-     let addSingle a s = add a 1u s
-     let remove a n (M s) =
+    let add a n (M s) =
+        match Map.tryFind a s with
+        | Some an -> Map.add a (an + n) s |> M
+        | None -> Map.add a n s |> M   
+    let addSingle a s = add a 1u s
+    let remove a n (M s) =
         match Map.tryFind a s with
         | Some an when an <= n -> Map.add a 0u s |> M
         | Some an -> Map.add a (an - n) s |> M
         | None -> s |> M
-     let removeSingle a (M s) = Map.remove a s |> M
-     let fold f acc (M s) = Map.fold f acc s
-     let foldBack f (M s) acc = Map.foldBack f s acc
-     
-     //Yellow
-     //let ofList lst = List.fold (fun acc x -> addSingle x acc) empty lst
-     //let toList (M s) = fold 
+    let removeSingle a (M s) =
+        match Map.tryFind a s with
+        | Some an -> Map.add a (an - 1u) s |> M
+        | None -> s |> M
+        
+        
+        //Map.add a -1u s |> M
+    let fold f acc (M s) = Map.fold f acc s
+    let foldBack f (M s) acc = Map.foldBack f s acc
+         
+         //Yellow
+         //let ofList lst = List.fold (fun acc x -> addSingle x acc) empty lst
+         //let toList (M s) = fold
+         
+         
--- Assignment5/Assignment5/Program.fs
+++ Assignment5/Assignment5/Program.fs
@@ -1,9 +1,11 @@
-﻿//Exercise 5.1
-let rec sum acc n =
-    match n with
-    | 0 -> acc
-    | _ -> sum (acc + n) (n - 1)
-
+﻿//Green
+//Exercise 5.1  
+let sum m n =
+    let rec aux acc =
+        function
+        | 0 -> acc + m
+        | n -> aux (acc + (m + n)) (n - 1)
+    aux 0 n
 
 //Exercise 5.2
 let length lst =
@@ -13,12 +15,13 @@ let length lst =
         | x::xs -> lengthA (acc + 1) xs
     lengthA 0 lst
     
-//Exercise 5.3
-let rec foldBack f acc lst =
-    match lst with
-    | [] -> f acc
-    | x::xs -> foldBack f (foldBack f xs acc)
-//Need help here
+//Exercise 5.3    
+let foldBack folder lst acc =
+    let rec foldBackC lst c =
+        match lst with
+        | [] -> c acc
+        | x :: xs -> foldBackC xs (fun r -> c (folder x r))
+    foldBackC lst id
 
 //Exercise 5.4
 let factA x =
@@ -28,5 +31,34 @@ let factA x =
         | x -> aux (x * acc) (x - 1)
     aux 1 x
     
+
 let factC x =
-    let rec aux 
+    let rec aux c =
+        function
+        | 0 -> c 1
+        | n -> aux (fun r -> c (r * n)) (n - 1) // r = fact (n - 1) * n
+    aux id x
+    
+//Yellow
+//Exercise 5.5
+let fibA n =
+    let rec aux acc1 acc2 =
+        function
+        | 0 -> acc1
+        | x -> aux acc2 (acc1 + acc2) (x - 1)
+    aux 0 1 n
+
+let fibC n =
+    let rec aux c =
+        function
+        | 0 -> fst (c (0, 1))
+        | x -> aux (fun (acc1, acc2) -> c (acc2, (acc1 + acc2))) (x - 1)
+    aux id n
+
+//Exercise 5.6
+let rec bigListK c =
+    function
+    | 0 -> c []
+    | n -> bigListK (fun res -> 1 :: c res) (n - 1)
+    
+bigListK id 130000
